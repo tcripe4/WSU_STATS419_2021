202a11267e201dea700dad3c8a5a313bcaaaae1f
yeet <cripe21@gmail.com>
2021-02-09 13:35:51 -0800

    Hello-world notebook push

---
+++ functions/multiply.cpp
@@ -0,0 +1,32 @@
+// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
+
+#include <RcppArmadillo.h>
+#include <RcppEigen.h>
+
+// [[Rcpp::export]]
+SEXP armaMatMult(arma::mat A, arma::mat B){
+    arma::mat C = A * B;
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMatTrans(Eigen::MatrixXd A){
+    Eigen::MatrixXd C = A.transpose();
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMatMult(Eigen::MatrixXd A, Eigen::MatrixXd B){
+    Eigen::MatrixXd C = A * B;
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+    Eigen::MatrixXd C = A * B;
+
+    return Rcpp::wrap(C);
+}
